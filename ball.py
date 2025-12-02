786dc81f5df79a0890504806011c2fc42dabd945
TU-E320-24 <>
2025-12-02 17:43:58 +0900

    ball.py 작성

---
+++ ball.py
@@ -0,0 +1,20 @@
+from pico2d import load_image, get_canvas_width, get_canvas_height, clamp
+import common
+
+class Ball:
+    def __init__(self, x, y):
+        self.image = load_image('ball21x21.png')
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
