8ccd8d2e307b6691d2a9e1fc28009a3829071754
TU-E320-24 <>
2025-12-02 17:49:54 +0900

    ball과 boy 충돌시에 공을 먹고 공이 world에서 사라짐

--- ball.py
+++ ball.py
@@ -1,5 +1,7 @@
 from pico2d import load_image, get_canvas_width, get_canvas_height, clamp
 import common
+import game_world
+
 
 class Ball:
     def __init__(self, x, y):
@@ -12,8 +14,10 @@ class Ball:
         sy = self.y - common.court.window_bottom
         self.image.draw(sx, sy)
 
-    def handle_collision(self, group, other):
-        pass
+    def handle_collision(self, key, other):
+        if key == 'boy:ball':
+            common.balls.remove(self)
+            game_world.remove_object(self)
 
     def update(self):
         pass
